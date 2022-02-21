d7999b9f24ab824d59b77763991bd12394111830
Marcelo Humerez Mars <mhumerez@tecnimotors.com>
2022-02-21 01:10:51 -0500

    Proyecto terminado y operativo, incluye Directorio Dist

--- src/app/weather.ts
+++ src/app/weather.ts
@@ -2,11 +2,14 @@ export interface Weather {
     weather: WeatherInfo[],
     main: {
       temp: number;
+      temp_min: number;
+      temp_max: number;
       pressure: number;
       humidity: number;
     };
     wind: {
       speed: number;
+      deg: number;
     };
     sys: {
       country: string
